b71bcb848282b3ea6a4debece80072aa5d9d756d
ayushgoyal09 <ayushgoyal09@gmail.com>
2014-03-19 14:50:54 -0700

    Created Android project. Added layout for the login screen, created Main Activity Login.java

---
+++ SFSUSystems/gen/com/sfsu/sfsusystems/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sfsu.sfsusystems;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ SFSUSystems/src/com/sfsu/systems/Login.java
@@ -0,0 +1,15 @@
+package com.sfsu.systems;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.sfsu.sfsusystems.R;
+
+public class Login extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+	}
+}
