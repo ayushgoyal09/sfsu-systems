9c1511819f8dc34ce19a7468a1af3243f42ce414
ayushgoyal09 <ayushgoyal09@gmail.com>
2014-04-29 19:53:55 -0700

    adding layout and activity file for Add owners.

---
+++ SFSUSystems/src/com/sfsu/system/inventory/add/AddOwners.java
@@ -0,0 +1,17 @@
+package com.sfsu.system.inventory.add;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.sfsu.sfsusystems.R;
+
+public class AddOwners extends Activity{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_owners);
+	}
+
+}
